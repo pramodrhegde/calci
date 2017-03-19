554b99c075805a696de25e647e50f213aff09adc
pramodrhegde <pramod.rh@gmail.com>
2017-03-20 00:23:15 +0530

    crashalytics added

--- android/app/src/main/java/com/calci/MainApplication.java
+++ android/app/src/main/java/com/calci/MainApplication.java
@@ -3,6 +3,7 @@ package com.prazor9.calci;
 import android.app.Application;
 import android.util.Log;
 
+import com.crashlytics.android.Crashlytics;
 import com.facebook.react.ReactApplication;
 import com.zmxv.RNSound.RNSoundPackage;
 import com.facebook.react.ReactInstanceManager;
@@ -11,8 +12,11 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import io.fabric.sdk.android.Fabric;
 import java.util.Arrays;
 import java.util.List;
+
+import com.smixx.fabric.FabricPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -25,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
+          new FabricPackage(),
           new MainReactPackage(),
             new RNSoundPackage()
       );
@@ -39,6 +44,7 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+    Fabric.with(this, new Crashlytics());
     SoLoader.init(this, /* native exopackage */ false);
   }
 }
